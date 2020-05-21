d2436fdf7aa84a7cc44144cccfe83e143c6a5c0e
zhangya123 <2282532900@qq.com>
2020-05-21 20:35:35 +0800

    Site updated: 2020-05-21 20:35:27

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Thu May 21 2020 20:35:23 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
